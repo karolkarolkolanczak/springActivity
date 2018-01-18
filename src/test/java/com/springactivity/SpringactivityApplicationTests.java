071f49a10a3ba9b430c8db4d5554fab2df9b18bd
Karol <karol.kolanczak@hotmail.com>
2018-01-18 20:14:23 +0100

    initial commit

---
+++ src/test/java/com/springactivity/SpringactivityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springactivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringactivityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
