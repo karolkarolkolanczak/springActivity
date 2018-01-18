d4bcf44d2058455e8979958b9d34d1c8b7b6c95d
Karol <karol.kolanczak@hotmail.com>
2018-01-18 21:56:21 +0100

    adding bootstrap layout template

--- src/main/java/com/springactivity/SpringactivityApplication.java
+++ src/main/java/com/springactivity/SpringactivityApplication.java
@@ -2,11 +2,14 @@ package com.springactivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringactivityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringactivityApplication.class, args);
+
+		ApplicationContext ctx=SpringApplication.run(SpringactivityApplication.class, args);
+
 	}
 }
--- src/main/java/com/springactivity/controllers/IndexController.java
+++ src/main/java/com/springactivity/controllers/IndexController.java
@@ -1,7 +1,17 @@
 package com.springactivity.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by a on 18/01/2018.
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("OKKKK");
+        return "index";
+    }
 }
