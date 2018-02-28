4e5869e318e5dff07e234f8a052304ff2539462e
Karol <karol.kolanczak@hotmail.com>
2018-02-28 22:30:44 +0100

    heroku config postrges - online server config nr 19

--- src/main/java/com/springactivity/model/Product.java
+++ src/main/java/com/springactivity/model/Product.java
@@ -23,7 +23,7 @@ public class Product implements Serializable {
     private BigDecimal price;
     private String fotoImageUrl;
 //    @Lob @Basic(fetch = FetchType.EAGER)
-    @Type(type="org.hibernate.type.PrimitiveByteArrayBlobType")
+@Type(type="org.hibernate.type.BinaryType")
     private Byte[] image;
     @Transient
     private MultipartFile file;
